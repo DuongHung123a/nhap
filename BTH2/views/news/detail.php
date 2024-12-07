6c08214a74d4e80bc1860c3697271288d1891831
DuongTienDat3011 <144588056+DuongTienDat3011@users.noreply.github.com>
2024-12-07 08:45:04 +0700

    Update detail.php

--- BTH2/views/news/detail.php
+++ BTH2/views/news/detail.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Chi tiết tin tức</title>
+    <title><?= $news['title'] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
-    <h1><?= $news['title'] ?></h1>
-    <img src="<?= $news['image'] ?>" class="img-fluid mb-4" alt="Ảnh bài viết">
+    <h1 class="mb-4"><?= $news['title'] ?></h1>
+    <img src="<?= $news['image'] ?>" class="img-fluid mb-4" alt="Hình ảnh tin tức">
     <p><?= $news['content'] ?></p>
     <a href="index.php" class="btn btn-secondary mt-4">Quay lại danh sách</a>
 </div>
